f7af321bb1c554c6f5fd1da8ba190b3619fc0906
Lucas Ferreira valente <valentelucas85@gmail.com>
2022-11-25 12:37:24 -0300

    save point

---
+++ src/Components/RadioButton/index.tsx
@@ -0,0 +1,42 @@
+import { Container, FormControl, Radio, WarningOutlineIcon } from 'native-base';
+import React from 'react';
+
+const RadioButton = () => {
+    const [groupValue, setGroupValue] = React.useState("1");
+    return (
+        <Container>
+            <FormControl isInvalid>
+
+                <FormControl.Label _text={{
+                    fontSize: "lg",
+                    bold: true
+                }}>
+
+                    Select Prize
+
+                </FormControl.Label>
+
+                <Radio.Group name="exampleGroup" accessibilityLabel="select prize" defaultValue={groupValue} onChange={value => {
+                    setGroupValue(value || "");
+                }}>
+
+                    <Radio value="1" my="1">
+                        First
+                    </Radio>
+                    <Radio value="2" my="1">
+                        Second
+                    </Radio>
+                    <Radio value="3" my="1">
+                        Third
+                    </Radio>
+
+                </Radio.Group>
+
+                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                    You must select a Prize.
+                </FormControl.ErrorMessage>
+
+            </FormControl>
+        </Container>
+    )
+};
